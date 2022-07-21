bb4564c7cb586c646d758af3227940d9188c5250
Sofia <36406321+SofjaIRM@users.noreply.github.com>
2022-07-21 10:42:54 +0100

    Refactore code

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,203 +5,203 @@ class TodoList extends Component {
   constructor(props){
     super(props);
     this.state = {
-      tarefas:[],
-      tarefas_done:[],
-      tarefa_adicionar: "",
-      estado_tarefa: "",
-      data_tarefa: new Date().toISOString().slice(0, 10),
-      alerta_tarefa: "",
+      tasks:[],
+      tasksDone:[],
+      currentTask: "",
+      done: false,
+      date: new Date().toISOString().slice(0, 10),
+      alert: "",
       show_alert: "hidden_alert",
-      wrapper_tarefas_done: "show",
-      buttonShowHide: "Ocultar tarefas concluidas"
+      tasksDoneClassName: 'show',
+      buttonShowHide: "Hide finished tasks",
+      taskEditing: -1
     };
 
     this.handleInputTextChange = this.handleInputTextChange.bind(this);
     this.handleInputDateChange = this.handleInputDateChange.bind(this);
     this.handleSubmitForm = this.handleSubmitForm.bind(this);
-    this.handleHideTarefasDone = this.handleHideTarefasDone.bind(this);
-
+    this.handleHideTasksDone = this.handleHideTasksDone.bind(this);
   };
 
   componentWillMount (){
-    this.getLocalTarefas();
+    this.getLocalTasks();
     this.getLocalDone()
-    //this.setLocalTarefas([]);
-    //this.setLocalDone([]);
   }
   componentDidMount (){
-    this.tarefa_adicionar.focus()
+    this.currentTask.focus()
   }
 
   shouldComponentUpdate (){
     return true;
   }
 
-  //adicionar nova tarefa
+  // add new task
   handleSubmitForm(e){
     e.preventDefault()
-    //verifica se a tarefa e a data estão preenchidas
-    if(this.state.tarefa_adicionar && this.state.data_tarefa !== ""){
-      this.state.tarefas.push({
-        text: this.state.tarefa_adicionar,
-        date: this.state.data_tarefa,
-        done: this.state.estado_tarefa === "not_done" ? true : false,
+    // check if task and date are filled
+    if(this.state.currentTask && this.state.date !== ""){
+      this.state.tasks.push({
+        text: this.state.currentTask,
+        date: this.state.date,
+        done: this.state.done === "not_done" ? true : false,
       })
-      //reinicializa input após inserir tarefa
+
+      // restart input after add new task
       this.setState({
-        tarefas: this.state.tarefas,
-        tarefa_adicionar: "",
-        data_tarefa: ""
+        tasks: this.state.tasks,
+        currentTask: "",
+        date: ""
       })
-      //oculta pussíveis mensagens de alerta
+      // hide alert messages
       this.setState({
         show_alert: "hidden"
       })
-      //guarda tarefas localmente
-      this.setLocalTarefas(this.state.tarefas)
-      this.tarefa_adicionar.focus()
+      // save tasks locally
+      this.setLocalTasks(this.state.tasks)
+      this.currentTask.focus()
     }
-    //alerta, tarefa não inserida
-    else if(this.state.tarefa_adicionar === "" && this.state.data_tarefa !== ""){
+    // alert, task not filled
+    else if(this.state.currentTask === ""){
       this.setState({
-        alerta_tarefa: "Insira uma tarefa",
+        alert: "You must insert a task",
         show_alert: "show_alert"
       })
     }
-    //alerta, data não inserida
-    else if(this.state.tarefa_adicionar !== "" && this.state.data_tarefa === ""){
+    // alert, date no filled
+    else if(this.state.currentTask !== ""){
       this.setState({
-        alerta_tarefa: "Insira uma data",
+        alert: "You must insert a date",
         show_alert: "show_alert"
       })
     }
   }
 
-  //input nome da tarefa
+  // input name
   handleInputTextChange(e){
     this.setState({
       [e.target.name]: e.target.value
     })
   }
-  //input data da tarefa
+  // input date
   handleInputDateChange(e){
     this.setState({
       [e.target.name]: e.target.value
     })
   }
-  //switch tarefa (checked)
-  handleToggleDone(tarefaIndex, e){
-    let tarefas = this.state.tarefas
-    let tarefas_done = this.state.tarefas_done
-    tarefas[tarefaIndex].done = !this.state.tarefas[tarefaIndex].done
-    tarefas_done.unshift(this.state.tarefas[tarefaIndex])
-    tarefas.splice(tarefaIndex, 1)
-    this.setState({tarefas_done})
-    this.setLocalTarefas(this.state.tarefas)
-    this.setLocalDone(this.state.tarefas_done)
+  // switch task (checked)
+  handleToggleDone(taskIndex, e){
+    let tasks = this.state.tasks
+    let tasksDone = this.state.tasksDone
+    tasks[taskIndex].done = !this.state.tasks[taskIndex].done
+    tasksDone.unshift(this.state.tasks[taskIndex])
+    tasks.splice(taskIndex, 1)
+    this.setState({tasksDone})
+    this.setLocalTasks(this.state.tasks)
+    this.setLocalDone(this.state.tasksDone)
   }
-  //switch tarefa (unchecked)
-  handleToggleNotDone(tarefaIndex, e){
-    let tarefas_done = this.state.tarefas_done
-    let tarefas = this.state.tarefas
-    tarefas_done[tarefaIndex].done = !this.state.tarefas_done[tarefaIndex].done
-    tarefas.unshift(this.state.tarefas_done[tarefaIndex])
-    tarefas_done.splice(tarefaIndex, 1)
+  //switch task (unchecked)
+  handleToggleNotDone(taskIndex, e){
+    let tasksDone = this.state.tasksDone
+    let tasks = this.state.tasks
+    tasksDone[taskIndex].done = !this.state.tasksDone[taskIndex].done
+    tasks.unshift(this.state.tasksDone[taskIndex])
+    tasksDone.splice(taskIndex, 1)
     this.setState({
-      tarefas: this.state.tarefas,
-      tarefas_done: this.state.tarefas_done
+      tasks: this.state.tasks,
+      tasksDone: this.state.tasksDone
     })
-    this.setLocalTarefas(this.state.tarefas)
-    this.setLocalDone(this.state.tarefas_done)
+    this.setLocalTasks(this.state.tasks)
+    this.setLocalDone(this.state.tasksDone)
   }
-  //ativar edição de tarefa
-  handleEdit(tarefaIndex, e){
+  // enable edit task
+  handleEdit(taskIndex, e){
     this.setState({
-      tarefa_editing: this.state.tarefa_editing === tarefaIndex ? null : tarefaIndex
+      taskEditing: this.state.taskEditing === taskIndex ? null : taskIndex
     })
   }
-  //input editar tarefa
-  handleChangeTarefaName(tarefaIndex, e){
-    let tarefas = this.state.tarefas
-    tarefas[tarefaIndex].text = e.target.value
+  // edit task input
+  handleChangeTaskName(taskIndex, e){
+    let tasks = this.state.tasks
+    tasks[taskIndex].text = e.target.value
     this.setState({
-      tarefas
+      tasks
     })
-    this.setLocalTarefas(this.state.tarefas)
-    this.setLocalDone(this.state.tarefas_done)
+    this.setLocalTasks(this.state.tasks)
+    this.setLocalDone(this.state.tasksDone)
   }
-  //input editar data
-  handleChangeTarefaDate(tarefaIndex, e){
-    let tarefas = this.state.tarefas
-    tarefas[tarefaIndex].date = e.target.value
+
+  // edit date input
+  handleChangeTaskDate(taskIndex, e){
+    let tasks = this.state.tasks
+    tasks[taskIndex].date = e.target.value
     this.setState({
-      tarefas: this.state.tarefas
+      tasks: this.state.tasks
     })
-    this.setLocalTarefas(this.state.tarefas)
-    this.setLocalDone(this.state.tarefas_done)
+    this.setLocalTasks(this.state.tasks)
+    this.setLocalDone(this.state.tasksDone)
   }
-  //remover tarefas (unchecked)
-  handleRemove(tarefaIndex, e){
-    this.state.tarefas.splice(tarefaIndex, 1)
-    this.setState({tarefas: this.state.tarefas})
-    this.setLocalTarefas(this.state.tarefas)
-    this.setLocalDone(this.state.tarefas_done)
+
+  // remove task (unchecked)
+  handleRemove(taskIndex, e){
+    this.state.tasks.splice(taskIndex, 1)
+    this.setState({tasks: this.state.tasks})
+    this.setLocalTasks(this.state.tasks)
+    this.setLocalDone(this.state.tasksDone)
   }
-  //remover tarefas (checked)
-  handleRemoveDone(tarefaIndex, e){
-    this.state.tarefas_done.splice(tarefaIndex, 1)
-    this.setState({tarefas_done: this.state.tarefas_done})
-    this.setLocalTarefas(this.state.tarefas)
-    this.setLocalDone(this.state.tarefas_done)
+  // remove task (checked)
+  handleRemoveDone(taskIndex, e){
+    this.state.tasksDone.splice(taskIndex, 1)
+    this.setState({tasksDone: this.state.tasksDone})
+    this.setLocalTasks(this.state.tasks)
+    this.setLocalDone(this.state.tasksDone)
   }
-  //show hide tarefas done
-  handleHideTarefasDone(){
-    if(this.state.wrapper_tarefas_done === "show"){
+  // show hide finished task
+  handleHideTasksDone(){
+    if(this.state.tasksDoneClassName === "show"){
       this.setState({
-        wrapper_tarefas_done: "hidden",
-        buttonShowHide: "Mostrar tarefas concluidas"
+        tasksDoneClassName: "hidden",
+        buttonShowHide: "Show finished tasks"
       })
-    }else{
+    } else {
       this.setState({
-        wrapper_tarefas_done: "show",
-        buttonShowHide: "Ocultar tarefas concluidas"
+        tasksDoneClassName: "show",
+        buttonShowHide: "Hide finished tasks"
       })
     }
   }
-  //guardar tarefas localmente
-  setLocalTarefas(tarefas){
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+  // save tasks locally
+  setLocalTasks(tasks){
+    localStorage.setItem("tasks", JSON.stringify(tasks))
   }
 
-  //guarda tarefas_done localmente
-  setLocalDone(tarefas){
-    localStorage.setItem("tarefas_done", JSON.stringify(tarefas))
+  // save finished tasks locally
+  setLocalDone(tasks){
+    localStorage.setItem("tasksDone", JSON.stringify(tasks))
   }
 
-  //receber tarefas guardadas localmente
-  getLocalTarefas(){
-    let tarefas = localStorage.getItem("tarefas")
-    if(tarefas === null){
-      tarefas = [];
+  // get finished tasks locally
+  getLocalTasks(){
+    let tasks = localStorage.getItem("tasks")
+    if(tasks === null){
+      tasks = [];
     }else{
-      tarefas = JSON.parse(tarefas)
+      tasks = JSON.parse(tasks)
     }
-    this.setState({tarefas})
+    this.setState({tasks})
   }
 
-  //recebe tarefas_done guardadas localmente
+  // get finished tasks saved locally
   getLocalDone(){
-    let tarefas_done = localStorage.getItem("tarefas_done")
-    if(tarefas_done === null){
-      tarefas_done = [];
-    }else{
-      tarefas_done = JSON.parse(tarefas_done)
+    let tasksDone = localStorage.getItem("tasksDone")
+    if(tasksDone === null) {
+      tasksDone = [];
+    } else {
+      tasksDone = JSON.parse(tasksDone)
     }
-    this.setState({tarefas_done})
+    this.setState({tasksDone})
   }
 
   render() {
-    console.log(this.state.data_tarefa);
     return (
       <div className="TodoList">
         <div>
@@ -210,77 +210,95 @@ class TodoList extends Component {
         <div className="wrapper-input">
           <form onSubmit={this.handleSubmitForm}>
             <input
-              type="text" name="tarefa_adicionar"
-              placeholder="Insira uma tarefa..."
-              value={this.state.tarefa_adicionar}
+              type="text" name="currentTask"
+              placeholder="Add a new task..."
+              value={this.state.currentTask}
               onChange={this.handleInputTextChange}
-              ref={(el) => {this.tarefa_adicionar = el}}
+              ref={(el) => {this.currentTask = el}}
             />
-            <input type="date"
-                   value={this.state.data_tarefa}
-                   name="data_tarefa"
-                   min={Date.now()}
-                   onChange={this.handleInputDateChange}/>
-            <button className="adicionar-tarefa"
-                    type="submit"
-                    value={this.state.tarefa_adicionar}>
-                    Adicionar Tarefa
+            <input
+              type="date"
+              value={this.state.date}
+              name="date"
+              min={Date.now()}
+              onChange={this.handleInputDateChange}
+            />
+            <button
+              className="add-task"
+              type="submit"
+              value={this.state.currentTask}
+            >
+              Add Task
             </button>
           </form>
-          <span className={this.state.show_alert}>{this.state.alerta_tarefa}</span>
+          <span className={this.state.show_alert}>{this.state.alert}</span>
         </div>
         <button className="showHide-done"
-                onClick={this.handleHideTarefasDone}>
+                onClick={this.handleHideTasksDone}>
                 {this.state.buttonShowHide}
         </button>
-        <div className="wrapper_tarefas">
+        <div className="wrapper_tasks">
           <ul>
-            {this.state.tarefas.map((item, index) => {
-              return (
-                <li key={"tarefa" + index} className="not_done">
-                  <button className="far fa-square fa-lg tarefa-uncheck"
-                          onClick={this.handleToggleDone.bind(this, index)}/>
+            {this.state.tasks.map((item, index) => {
+                this.isEditing = this.state.taskEditing === index;
+                console.log(this.state.taskEditing);
+
+                return <li key={"task" + index} className="not_done">
+                  <button
+                    className="far fa-square fa-lg task-uncheck"
+                    onClick={this.handleToggleDone.bind(this, index)}
+                  />
                   {
-                    (this.state.tarefa_editing === index)
-                    ? <input onChange={this.handleChangeTarefaName.bind(this, index)}
-                              value={item.text}/>
-                    : <span>{item.text}</span>
+                    (this.isEditing)
+                    ? <>
+                        <input
+                          onChange={this.handleChangeTaskName.bind(this, index)}
+                          value={item.text}
+                        />
+                        <input
+                          type="date"
+                          onChange={this.handleChangeTaskDate.bind(this, index)}
+                          value={item.date}
+                        />
+                      </>
+                    : <>
+                        <span>{item.text}</span>
+                        <input type="date"
+                          readOnly={true}
+                          onChange={this.handleChangeTaskDate.bind(this, index)}
+                          value={item.date}
+                        />
+                      </>
                   }
-                  {
-                    (this.state.tarefa_editing === index)
-                    ? <input type="date"
-                              onChange={this.handleChangeTarefaDate.bind(this, index)}
-                              value={item.date}/>
-                    : <input type="date"
-                              readOnly={true}
-                              onChange={this.handleChangeTarefaDate.bind(this, index)}
-                              value={item.date}/>
-                  }
-                  <button className={(this.state.tarefa_editing === index)
-                                      ? "fas fa-save fa-lg tarefa-guardar"
-                                      : "fas fa-edit fa-lg tarefa-edit"}
-                          onClick={this.handleEdit.bind(this, index)}/>
-                  <button className="fas fa-trash-alt fa-lg tarefa-delete"
-                          onClick={this.handleRemove.bind(this, index)}
-                          disabled={(!isNaN (this.state.tarefa_editing) && this.state.tarefa_editing !== null)
-                                      ? "disabled"
-                                      : ""}/>
+                  <button
+                    className={(this.isEditing)
+                      ? "fas fa-save fa-lg task-save"
+                      : "fas fa-edit fa-lg task-edit"}
+                      onClick={this.handleEdit.bind(this, index)}
+                  />
+                  <button
+                    className="fas fa-trash-alt fa-lg task-delete"
+                    onClick={this.handleRemove.bind(this, index)}
+                  />
                 </li>
-                )
             })}
           </ul>
         </div>
-        <div className="wrapper_tarefas_done">
-          <ul className={this.state.wrapper_tarefas_done}>
+        <div className="wrapper_tasks_done show">
+          <ul className={this.state.tasksDoneClassName}>
             {
-              this.state.tarefas_done.map((item, index) => {
+              this.state.tasksDone.map((item, index) => {
                 return (
-                  <li key={"tarefa" + index} className="done">
-                    <button className="far fa-check-square fa-lg tarefa-uncheck"
-                            onClick={this.handleToggleNotDone.bind(this, index)}/>
+                  <li key={"task" + index} className="done">
+                    <button
+                      className="far fa-check-square fa-lg task-uncheck"
+                      onClick={this.handleToggleNotDone.bind(this, index)}
+                    />
                     <span>{item.text}</span>
-                    <button className="fas fa-trash-alt fa-lg tarefa-delete"
-                            onClick={this.handleRemoveDone.bind(this, index)}/>
+                    <button
+                      className="fas fa-trash-alt fa-lg task-delete"
+                      onClick={this.handleRemoveDone.bind(this, index)}
+                    />
                   </li>
                 )
               })
